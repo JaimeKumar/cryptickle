573e31e1f48b2413e106f37d20f48ec8d9fbc968
Jaime Kumar <jaimelacree@gmail.com>
2023-05-11 22:27:25 +0100

    resize function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import $ from 'jquery';
 
+
+function resize() {
+  $('.bodyCont').css('height', window.innerHeight + 'px');
+}
+
+window.addEventListener('resize', resize);
+resize();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
